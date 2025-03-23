8d99069d424a7a3cebdc7f5d2d382a88a3ba647e
Memo230701 <m.h.yenilmez@gmail.com>
2025-03-23 14:46:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Einfacher Konsolen-Check
+console.log("Website von Mehmet ist geladen.");
+
+// (Optional) Interaktive Aktion beim Button
+document.querySelector('.btn').addEventListener('click', () => {
+  alert('Mehr Infos folgen bald – Mehmet arbeitet dran! 🚀');
+});
+
+
