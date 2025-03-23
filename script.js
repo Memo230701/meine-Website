823b85cc4f0d45ab0883c0187ffd00f26378dab6
Memo230701 <m.h.yenilmez@gmail.com>
2025-03-23 15:15:06 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-console.log("Website von Mehmet wurde geladen.");
-
-// Optional: Interaktion mit dem Button
-document.querySelector('.btn').addEventListener('click', () => {
-  alert('Scroll nach unten, um mehr über mich zu erfahren 🚀');
-});
